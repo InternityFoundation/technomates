c738fa46c99775c7fff9084aebb979cdd5453e35
Akash156 <46242560+Akash156@users.noreply.github.com>
2019-01-12 22:22:33 +0530

    Add files via upload

---
+++ TEST321.ino
@@ -0,0 +1,60 @@
+
+#define USE_ARDUINO_INTERRUPTS true    // Set-up low-level interrupts for most acurate BPM math.
+#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+const int PulseWire = 0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+const int LED13 = 13;          // The on-board Arduino LED, close to PIN 13.
+int Threshold = 550;           // Determine which Signal to "count as a beat" and which to ignore.
+                               // Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
+                               // Otherwise leave the default "550" value. 
+                               
+PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
+LiquidCrystal_I2C lcd(0x3f,16,2);  // set the LCD address to 0x3f for a 16 chars and 2 line display
+
+
+void setup() {   
+
+  Serial.begin(9600);          // For Serial Monitor
+
+ lcd.init();                      // initialize the lcd 
+  lcd.init();
+  // Print characters on LCD.
+  lcd.backlight();
+  
+  // Configure the PulseSensor object, by assigning our variables to it. 
+  pulseSensor.analogInput(PulseWire);   
+  pulseSensor.blinkOnPulse(LED13);       //auto-magically blink Arduino's LED with heartbeat.
+  pulseSensor.setThreshold(Threshold);   
+
+  // Double-check the "pulseSensor" object was created and "began" seeing a signal. 
+   if (pulseSensor.begin()) {
+    Serial.println("We created a pulseSensor Object !");  //This prints one time at Arduino power-up,  or on Arduino reset.  
+  }
+}
+
+
+
+void loop() {
+
+ int myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
+                                               // "myBPM" hold this BPM value now. 
+
+if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened". 
+ Serial.println("♥  A HeartBeat Happened ! "); // If test is "true", print a message "a heartbeat happened".
+ Serial.print("BPM: ");                        // Print phrase "BPM: " 
+ Serial.println(myBPM);       // Print the value inside of myBPM. 
+ 
+  lcd.setCursor(0, 1);
+  lcd.print("HB -> ");
+  lcd.setCursor(14, 1);
+  lcd.print(myBPM);
+  lcd.setCursor(7, 1);
+  lcd.print("BPM");
+}
+
+  delay(10);                    // considered best practice in a simple sketch.
+
+}
+
+  
